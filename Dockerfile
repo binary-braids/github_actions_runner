a8bee9cef55d4123649270889adb3f0c22965a72
Cuan Leo <cuanleo@gmail.com>
2024-04-20 10:08:45 +0200

    fix: reconfigure `start.sh` script to correctly accept optional environment variables

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,6 @@ RUN apt update -y && apt upgrade -y && useradd -m docker
 RUN apt install -y --no-install-recommends \
     curl jq build-essential libssl-dev libffi-dev python3 python3-venv python3-dev python3-pip
 
-
 RUN cd /home/docker && mkdir actions-runner && cd actions-runner \
     && curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz \
     && tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz
--- start.sh
+++ start.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$ACCESS_TOKEN
+ORGANIZATION="$ORGANIZATION"
+ACCESS_TOKEN="$ACCESS_TOKEN"
 
 # Set flags to control the --name option
-USE_HOSTNAME=false
-CUSTOM_NAME=""
+USE_HOSTNAME="${USE_HOSTNAME:-false}"  # Use default value 'false' if not set
+CUSTOM_NAME="${CUSTOM_NAME:-}"
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" "https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token" | jq .token --raw-output)
 
 # Specify the desired behavior based on flags
-if $USE_HOSTNAME && [ -n "$CUSTOM_NAME" ]; then
-    echo "error: --use-hostname and --custom-name are mutually exclusive"
-    exit 1
-elif $USE_HOSTNAME; then
-    # Use hostname as the runner name
-    NAME_OPTION="--name $(hostname)"
-elif [ -n "$CUSTOM_NAME" ]; then
-    # Use custom name specified by the user
-    NAME_OPTION="--name $CUSTOM_NAME"
-else
-    # Do not include --name option
-    NAME_OPTION=""
-fi
-
 cd /home/docker/actions-runner || exit
 
-./config.sh \
-    --url "https://github.com/${ORGANIZATION}" \
-    --token "$REG_TOKEN" \
-    "$NAME_OPTION"
+if [ "$USE_HOSTNAME" = "true" ] && [ -n "$CUSTOM_NAME" ]; then
+    echo "error: --use-hostname and --custom-name are mutually exclusive"
+    exit 1
+elif [ "$USE_HOSTNAME" = "true" ]; then
+    # Use hostname as the runner name
+    echo "USE_HOSTNAME selected. Name for runner: $(hostname)"
+    ./config.sh \
+        --url "https://github.com/${ORGANIZATION}" \
+        --token "$REG_TOKEN" \
+        --name "$(hostname)"
+elif [ -n "$CUSTOM_NAME" ]; then
+    echo "CUSTOM_NAME selected. Name for runner: $CUSTOM_NAME"
+    # Use custom name specified by the user
+    ./config.sh \
+        --url "https://github.com/${ORGANIZATION}" \
+        --token "$REG_TOKEN" \
+        --name "$CUSTOM_NAME"
+else
+    # Do not include --name option
+    ./config.sh \
+        --url "https://github.com/${ORGANIZATION}" \
+        --token "$REG_TOKEN"
+fi
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token "${REG_TOKEN}"
 }
 
 trap 'cleanup; exit 130' INT
